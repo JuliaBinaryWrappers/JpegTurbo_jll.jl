c321fd146e6fe4a1db349ccba25258a304941769
jlbuild <juliabuildbot@gmail.com>
2022-02-07 10:22:27 +0000

    JpegTurbo_jll build 2.1.2+0

--- src/wrappers/i686-linux-gnu.jl
+++ src/wrappers/i686-linux-gnu.jl
@@ -2,13 +2,25 @@
 export cjpeg, djpeg, jpegtran, libjpeg, libturbojpeg
 
 JLLWrappers.@generate_wrapper_header("JpegTurbo")
+JLLWrappers.@declare_library_product(libjpeg, "libjpeg.so.62")
+JLLWrappers.@declare_library_product(libturbojpeg, "libturbojpeg.so.0")
 JLLWrappers.@declare_executable_product(cjpeg)
 JLLWrappers.@declare_executable_product(djpeg)
 JLLWrappers.@declare_executable_product(jpegtran)
-JLLWrappers.@declare_library_product(libjpeg, "libjpeg.so.62")
-JLLWrappers.@declare_library_product(libturbojpeg, "libturbojpeg.so.0")
 function __init__()
     JLLWrappers.@generate_init_header()
+    JLLWrappers.@init_library_product(
+        libjpeg,
+        "lib/libjpeg.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_library_product(
+        libturbojpeg,
+        "lib/libturbojpeg.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
     JLLWrappers.@init_executable_product(
         cjpeg,
         "bin/cjpeg",
@@ -22,18 +34,6 @@ function __init__()
     JLLWrappers.@init_executable_product(
         jpegtran,
         "bin/jpegtran",
-    )
-
-    JLLWrappers.@init_library_product(
-        libjpeg,
-        "lib/libjpeg.so",
-        RTLD_LAZY | RTLD_DEEPBIND,
-    )
-
-    JLLWrappers.@init_library_product(
-        libturbojpeg,
-        "lib/libturbojpeg.so",
-        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@generate_init_footer()
